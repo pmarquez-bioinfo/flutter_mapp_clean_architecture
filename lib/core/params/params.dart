b271b1123a9651da7f0daff4b8b6c991b111aded
Pablo Marquez <pamarquez90@gmail.com>
2024-05-19 13:59:04 -0300

    feature(pokemon_image)

--- lib/core/params/params.dart
+++ lib/core/params/params.dart
@@ -8,3 +8,11 @@ class PokemonParams {
     required this.id,
   });
 }
+class PokemonImageParams {
+  final String name;
+  final String imageUrl;
+  const PokemonImageParams({
+    required this.name,
+    required this.imageUrl,
+  });
+}
---
+++ lib/features/pokemon_image/business/entities/pokemon_image_entity.dart
@@ -0,0 +1,6 @@
+class PokemonImageEntity {
+  final String path; // Local path to where the image is saved
+  const PokemonImageEntity({
+    required this.path,
+  });
+}
